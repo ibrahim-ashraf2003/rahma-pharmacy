b27870dab9f35ddaf922918c5c0458d2b5a03c73
ibrahim-ashraf2003 <ashra68109@gmail.com>
2026-08-17 10:16:38 +0300

    Restore NotFound page

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,1 @@
+import{Link}from'react-router-dom';export default function NotFound(){return <main className="p-16 text-center"><h1 className="text-5xl font-bold">404</h1><p className="my-4">Page not found</p><Link to="/" className="underline">Home</Link></main>}
